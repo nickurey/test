c678bc7e591a1e809b8af10c8ffdfbb839ea7d4b
Nickurey Huy Nguyen <nicky@localhost.local>
2015-11-24 02:24:16 +0800

    Initial Commit

---
+++ Assignment3_new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment3_new
+//
+//  Created by Nickurey Huy Nguyen on 11/24/15.
+//  Copyright © 2015 University Of Wollongong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
